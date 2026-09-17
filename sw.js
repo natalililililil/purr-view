3e64fd1f4bead30de90311253ed51ea9088cad91
Натали Жук <natalizuk09@gmail.com>
2026-09-17 15:12:55 +0300

    Refactor: switch caching strategy from Cache First to Network First

--- sw.js
+++ sw.js
@@ -27,16 +27,14 @@ self.addEventListener("activate", (e) => {
 self.addEventListener("fetch", (e) => {
   if (e.request.method !== "GET") return;
   e.respondWith(
-    caches.match(e.request).then(
-      (cached) =>
-        cached ||
-        fetch(e.request)
-          .then((res) => {
-            const copy = res.clone();
-            caches.open(CACHE).then((c) => c.put(e.request, copy));
-            return res;
-          })
-          .catch(() => cached)
-    )
+    fetch(e.request)
+      .then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE).then((c) => c.put(e.request, copy));
+        return response;
+      })
+      .catch(() => {
+        return caches.match(e.request);
+      })
   );
 });
